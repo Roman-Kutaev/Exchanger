4dc1a9f32190db1a2e515efc00f425134639a333
Roman Kutaev <Kutrd1310@gmail.com>
2021-04-05 15:19:43 +0300

    commit 4

--- src/main/java/com/example/exchanger/data/Request.java
+++ src/main/java/com/example/exchanger/data/Request.java
@@ -2,10 +2,7 @@ package com.example.exchanger.data;
 
 import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.math.BigDecimal;
 
 @Data
@@ -13,19 +10,23 @@ import java.math.BigDecimal;
 @RequiredArgsConstructor
 @AllArgsConstructor
 @Entity
+@Table(name = "requests")
 public class Request {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String cc;
     private String action;
+    @Column(name = "sum_currency")
     private BigDecimal sumCurrency;
     @NonNull
+    @Column(name = "phone_number")
     private String phoneNumber;
     private String status;
     @NonNull
+    @Column(name = "confirmation_code")
     private int confirmationCode;
     @NonNull
+    @Column(name = "sum_payment")
     private BigDecimal sumPayment;
-
 
 }
